a1f963417ae9956f242756f3a5b15758a2ea925a
denys <denamyte@gmail.com>
2023-08-31 22:41:13 +0300

    2023-08-31: 1 problem

--- Topics/Debugging techniques/Theory/src/Main.kt
+++ Topics/Debugging techniques/Theory/src/Main.kt
@@ -1,11 +1,7 @@
 // You can experiment here, it won’t be checked
 
 fun main(args: Array<String>) {
-  // put your code here
-}
-
-class Animal(val name: String) {
-    init {
-        check(name.isNotBlank()) { "Name must not be blank" }
-    }
+    mutableListOf("").size
+    mutableSetOf("").size
+    mutableMapOf("" to "").size
 }
